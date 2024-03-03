8057b7f5e76cdad91fc4c9c084cb857b042af529
Alim Weber <alim.weber@outlook.com>
2024-03-03 18:56:14 +0100

    gamestate on server

--- index.js
+++ index.js
@@ -4,5 +4,44 @@ const express = app.express;
 
 app.rest.use(express.static(path.join(__dirname, "dist")))
 
+
+
+let gameState = {
+	players:[]
+}
+
+let clients = []
+
+app.socket.on("register", (data) => {
+
+	let clientid = data.id;
+	
+
+	clients.push(clientid)
+
+
+
+	app.socket.send("playerRegistered", {
+		clientid,
+	}, clients.filter(id => id!= clientid))
+	
+	app.socket.send("getGameState", gameState, clientid)
+
+
+	gameState.players.push(clientid)
+})
+
+
+app.socket.on("clientUpdate", (data) => {
+
+	console.log(data)
+
+
+	
+
+	app.socket.send("serverUpdate", data, clients.filter(id => id!=data.clientid))
+
+})
+
 const env = require("./env.json")
 app.listen(env)
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,78 @@
 import { Actor, CollisionType, Color, Engine, Input, Keys, vec } from "excalibur";
 
 
+declare const socket :any;
+
+
+
+class OnlinePlayer extends Actor{
+	
+	public speed:number = .5; 
+	public playerid : string = ""
+	constructor(id:string){
+
+		super({
+			x:0,
+			y:0,
+			height:10,
+			width:10,
+			color:Color.Gray
+		})
+
+		this.playerid = id;
+	}
+	_onInitialize(_engine:Engine, delta:number){
+		this.color = Color.Gray	
+	}
+}
+
+socket.register() 
+
+
+socket.onopen(() => {
+
+	socket.sendToServer("register", {
+
+		id:socket.id
+
+	})
+
+})
+
+
 const game = new Engine({
 	backgroundColor:Color.White	
 })
 
-class OnlinePlayer extends Actor{
 
-
-}
 
 
 game.start()
+
+let onlineplayers : OnlinePlayer[] = []
+
+
+socket.on("playerRegistered", (data:any) => {
+
+
+	let {clientid} = data; 
+
+	let newPlayer = new OnlinePlayer(clientid)
+
+	onlineplayers.push(newPlayer)	
+
+	game.add(newPlayer)
+	
+})
+
+
+socket.on("serverUpdate", (data:any) => {
+
+	console.log(data, onlineplayers) 
+
+
+})
+
 
 class Player extends Actor{
 
@@ -36,19 +97,52 @@ class Player extends Actor{
 
 		if(game.input.keyboard.isHeld(Keys.W)){
 			this.pos.y -= this.speed * delta
+			socket.sendToServer("clientUpdate", {
+				clientid:socket.id,
+				update:{
+					pos:{
+						x:this.pos.x,
+						y:this.pos.y
+					}
+				}
+			})
 
 		}
 		if(game.input.keyboard.isHeld(Keys.S)){
 			this.pos.y += this.speed * delta
-
+			socket.sendToServer("clientUpdate", {
+				clientid:socket.id,
+				update:{
+					pos:{
+						x:this.pos.x,
+						y:this.pos.y
+					}
+				}
+			})
 		}
 		if(game.input.keyboard.isHeld(Keys.D)){
 			this.pos.x += this.speed * delta
-
+			socket.sendToServer("clientUpdate", {
+				clientid:socket.id,
+				update:{
+					pos:{
+						x:this.pos.x,
+						y:this.pos.y
+					}
+				}
+			})
 		}
 		if(game.input.keyboard.isHeld(Keys.A)){
 			this.pos.x -= this.speed * delta
-
+			socket.sendToServer("clientUpdate", {
+				clientid:socket.id,
+				update:{
+					pos:{
+						x:this.pos.x,
+						y:this.pos.y
+					}
+				}
+			})
 		}
 	}
 }
